72a2c53c392f8d53613329407eb462bf9471f48d
Stefan Bethke <stb@lassitu.de>
2021-06-12 18:03:17 +0200

    Hook up the keyboard

--- main.js
+++ main.js
@@ -1,27 +1,9 @@
 import Cept from './modules/cept.js';
 import Serial from './modules/websocketserial.js';
+import Keyboard from './modules/keyboard.js';
 
 window.addEventListener('DOMContentLoaded', (event) => {
   let cept = new Cept("#screen");
   let ws = new Serial(cept);
-  
-  // document.getElementById("charsAndColors").addEventListener("click", function(ev) {
-  //   ceptTest.charsAndColors();
-  // });
-  // document.getElementById("attrs").addEventListener("click", function(ev) {
-  //   ceptTest.attrs();
-  // });
-  // document.getElementById("charsetAlpha").addEventListener("click", function(ev) {
-  //   ceptTest.charsetAlpha();
-  // });
-  // document.getElementById("charsetMosaic").addEventListener("click", function(ev) {
-  //   ceptTest.charsetMosaic();
-  // });
-  // document.getElementById("bytestream").addEventListener("click", function(ev) {
-  //   ceptTest.bytestream();
-  // });
-  // document.getElementById("reveal").addEventListener("click", function(ev) {
-  //   cept.revealed = !cept.revealed;
-  //   cept.updateScreen();
-  // });
+  let kb = new Keyboard(ws);
 });
