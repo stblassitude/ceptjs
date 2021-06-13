70f9dfe8fa1ddddae6f1e9bc3cf8c84edff082fe
Stefan Bethke <stb@lassitu.de>
2021-06-14 00:29:12 +0200

    Add DRCS test

--- modules/cept-test.js
+++ modules/cept-test.js
@@ -16,7 +16,7 @@ export default class CeptTest {
   }
 
   moveTo(y, x) {
-    return [0x1f, y+0x40, x+0x40];
+    return [0x1f, y + 0x40, x + 0x40];
   }
 
   crlf() {
@@ -29,14 +29,14 @@ export default class CeptTest {
     for (var y = 0; y < this.cept.rows; y++) {
       this.cept.screen.rows[y].bg = y % 32;
       for (var x = 0; x < this.cept.cols; x++) {
-        this.cept.screen.rows[y].attr[x].bg = (this.cept.rows+x-y) % 32;
-        this.cept.screen.rows[y].attr[x].char = String.fromCharCode(64+x+y);
+        this.cept.screen.rows[y].attr[x].bg = (this.cept.rows + x - y) % 32;
+        this.cept.screen.rows[y].attr[x].char = String.fromCharCode(64 + x + y);
       }
     }
     for (var y = 10; y < 12; y++) {
       for (var x = 0; x < this.cept.cols; x++) {
         this.cept.screen.rows[y].attr[x].flashf = 1;
-        this.cept.screen.rows[y].attr[x].flashp = y-10;
+        this.cept.screen.rows[y].attr[x].flashp = y - 10;
         this.cept.screen.rows[y].attr[x].flashi = x >= 20;
         this.cept.screen.rows[y].attr[x].flashr = ~~(x / 10) % 2 == 1;
       }
@@ -44,9 +44,9 @@ export default class CeptTest {
     for (var y = 12; y < 15; y++) {
       for (var x = 0; x < this.cept.cols; x++) {
         this.cept.screen.rows[y].attr[x].flashf = 2;
-        this.cept.screen.rows[y].attr[x].flashp = y-12;
+        this.cept.screen.rows[y].attr[x].flashp = y - 12;
         this.cept.screen.rows[y].attr[x].flashi = x >= 20;
-        this.cept.screen.rows[y].attr[x].flashr = ~~(x / 10 ) % 2 == 1;
+        this.cept.screen.rows[y].attr[x].flashr = ~~(x / 10) % 2 == 1;
       }
     }
     this.cept.updateScreen();
@@ -195,7 +195,7 @@ export default class CeptTest {
     this.cept.bgColor = Cept.COLOR_REDUCED_INTENSITY_GREEN;
     this.cept.size = Cept.SIZE_DOUBLE_HEIGHT_ABOVE;
     this.cept.writeUnicode("DH/A");
-    this.cept.move(20, y-1);
+    this.cept.move(20, y - 1);
     this.cept.resetAttr();
     this.cept.color = Cept.COLOR_RED;
     this.cept.writeUnicode("hid");
@@ -204,7 +204,7 @@ export default class CeptTest {
     this.cept.color = Cept.COLOR_WHITE;
     this.cept.size = Cept.SIZE_DOUBLE_HEIGHT_BELOW;
     this.cept.writeUnicode("DH/B");
-    this.cept.move(30, y+1);
+    this.cept.move(30, y + 1);
     this.cept.resetAttr();
     this.cept.color = Cept.COLOR_RED;
     this.cept.writeUnicode("hid");
@@ -219,7 +219,7 @@ export default class CeptTest {
     this.cept.color = Cept.COLOR_WHITE;
     this.cept.size = Cept.SIZE_DOUBLE_SIZE;
     this.cept.writeUnicode("Doubl");
-    this.cept.move(20, y-1);
+    this.cept.move(20, y - 1);
     // y += 1;
     this.cept.resetAttr();
     this.cept.color = Cept.COLOR_RED;
@@ -246,7 +246,7 @@ export default class CeptTest {
     this.cept.resetAttr();
 
     for (var j = 0; j < 16; j++) {
-      this.cept.move(0, 7+j);
+      this.cept.move(0, 7 + j);
       this.cept.writeUnicode((" " + j).slice(-2));
     }
 
@@ -254,13 +254,13 @@ export default class CeptTest {
     this.cept.move(x, 4);
     this.cept.writeUnicode("Primary");
     for (var i = 0; i < 6; i++) {
-      this.cept.move(x + 2*i, 5);
+      this.cept.move(x + 2 * i, 5);
       this.cept.bgColor = Cept.COLOR_TRANSPARENT;
-      this.cept.writeUnicode("" + (i+2));
+      this.cept.writeUnicode("" + (i + 2));
       this.cept.bgColor = Cept.COLOR_GREY;
       for (var j = 0; j < 16; j++) {
-        this.cept.move(x + 2*i, 7+j);
-        this.cept.write(CeptCodeSets.PRIMARY[i*16+j]);
+        this.cept.move(x + 2 * i, 7 + j);
+        this.cept.write(CeptCodeSets.PRIMARY[i * 16 + j]);
       }
     }
 
@@ -269,13 +269,13 @@ export default class CeptTest {
     this.cept.resetAttr();
     this.cept.writeUnicode("Supplem.");
     for (var i = 0; i < 6; i++) {
-      this.cept.move(x + 2*i, 5);
+      this.cept.move(x + 2 * i, 5);
       this.cept.bgColor = Cept.COLOR_TRANSPARENT;
-      this.cept.writeUnicode("" + (i+2));
+      this.cept.writeUnicode("" + (i + 2));
       this.cept.bgColor = Cept.COLOR_GREY;
       for (var j = 0; j < 16; j++) {
-        this.cept.move(x + 2*i, 7+j);
-        this.cept.write(CeptCodeSets.SUPPLEMENTARY[i*16+j]);
+        this.cept.move(x + 2 * i, 7 + j);
+        this.cept.write(CeptCodeSets.SUPPLEMENTARY[i * 16 + j]);
       }
     }
 
@@ -284,13 +284,13 @@ export default class CeptTest {
     this.cept.resetAttr();
     this.cept.writeUnicode("Greek");
     for (var i = 0; i < 6; i++) {
-      this.cept.move(x + 2*i, 5);
+      this.cept.move(x + 2 * i, 5);
       this.cept.bgColor = Cept.COLOR_TRANSPARENT;
-      this.cept.writeUnicode("" + (i+2));
+      this.cept.writeUnicode("" + (i + 2));
       this.cept.bgColor = Cept.COLOR_GREY;
       for (var j = 0; j < 16; j++) {
-        this.cept.move(x + 2*i, 7+j);
-        this.cept.write(CeptCodeSets.GREEK[i*16+j]);
+        this.cept.move(x + 2 * i, 7 + j);
+        this.cept.write(CeptCodeSets.GREEK[i * 16 + j]);
       }
     }
   }
@@ -310,7 +310,7 @@ export default class CeptTest {
     this.cept.resetAttr();
 
     for (var j = 0; j < 16; j++) {
-      this.cept.move(0, 7+j);
+      this.cept.move(0, 7 + j);
       this.cept.writeUnicode((" " + j).slice(-2));
     }
 
@@ -319,53 +319,53 @@ export default class CeptTest {
     this.cept.resetAttr();
     this.cept.writeUnicode("Mosaic 1");
     for (var i = 0; i < 2; i++) {
-      this.cept.move(x + 2*i, 5);
+      this.cept.move(x + 2 * i, 5);
       this.cept.bgColor = Cept.COLOR_TRANSPARENT;
-      this.cept.writeUnicode("" + (i+2));
+      this.cept.writeUnicode("" + (i + 2));
       this.cept.bgColor = Cept.COLOR_GREY;
       for (var j = 0; j < 16; j++) {
-        this.cept.move(x + 2*i, 7+j);
-        this.cept.write(CeptCodeSets.SUPP_MOSAIC_1[i*16+j]);
+        this.cept.move(x + 2 * i, 7 + j);
+        this.cept.write(CeptCodeSets.SUPP_MOSAIC_1[i * 16 + j]);
       }
     }
     for (var i = 4; i < 6; i++) {
-      this.cept.move(x - 4 + 2*i, 5);
+      this.cept.move(x - 4 + 2 * i, 5);
       this.cept.bgColor = Cept.COLOR_TRANSPARENT;
-      this.cept.writeUnicode("" + (i+2));
+      this.cept.writeUnicode("" + (i + 2));
       this.cept.bgColor = Cept.COLOR_GREY;
       for (var j = 0; j < 16; j++) {
-        this.cept.move(x - 4 + 2*i, 7+j);
-        this.cept.write(CeptCodeSets.SUPP_MOSAIC_1[i*16+j]);
+        this.cept.move(x - 4 + 2 * i, 7 + j);
+        this.cept.write(CeptCodeSets.SUPP_MOSAIC_1[i * 16 + j]);
       }
     }
 
-    x += 2*4 + 1;
+    x += 2 * 4 + 1;
     this.cept.move(x, 4);
     this.cept.resetAttr();
     this.cept.writeUnicode("Mosaic 2");
     for (var i = 0; i < 6; i++) {
-      this.cept.move(x + 2*i, 5);
+      this.cept.move(x + 2 * i, 5);
       this.cept.bgColor = Cept.COLOR_TRANSPARENT;
-      this.cept.writeUnicode("" + (i+2));
+      this.cept.writeUnicode("" + (i + 2));
       this.cept.bgColor = Cept.COLOR_GREY;
       for (var j = 0; j < 16; j++) {
-        this.cept.move(x + 2*i, 7+j);
-        this.cept.write(CeptCodeSets.SUPP_MOSAIC_2[i*16+j]);
+        this.cept.move(x + 2 * i, 7 + j);
+        this.cept.write(CeptCodeSets.SUPP_MOSAIC_2[i * 16 + j]);
       }
     }
 
-    x += 2*6 + 1;
+    x += 2 * 6 + 1;
     this.cept.move(x, 4);
     this.cept.resetAttr();
     this.cept.writeUnicode("Mosaic 3");
     for (var i = 0; i < 4; i++) {
-      this.cept.move(x + 2*i, 5);
+      this.cept.move(x + 2 * i, 5);
       this.cept.bgColor = Cept.COLOR_TRANSPARENT;
-      this.cept.writeUnicode("" + (i+4));
+      this.cept.writeUnicode("" + (i + 4));
       this.cept.bgColor = Cept.COLOR_GREY;
       for (var j = 0; j < 16; j++) {
-        this.cept.move(x + 2*i, 7+j);
-        this.cept.write(CeptCodeSets.SUPP_MOSAIC_3[(i+2)*16+j]);
+        this.cept.move(x + 2 * i, 7 + j);
+        this.cept.write(CeptCodeSets.SUPP_MOSAIC_3[(i + 2) * 16 + j]);
       }
     }
   }
@@ -383,120 +383,130 @@ export default class CeptTest {
       0x8c, 0x87, 0x9e, // NSZ, WHF, TRB
     ]);
     bytes = bytes.concat(this.encodeString("Test Pages"));
-    bytes = bytes.concat(this.moveTo(1, 40-6));
+    bytes = bytes.concat(this.moveTo(1, 40 - 6));
     bytes = bytes.concat(this.encodeString("DM 0,00"));
-    bytes = bytes.concat([
-      0x1b, 0x23, 0x21, 0x54, // ESC 2/3 2/1 full row BLB
-      0x0a,
-      0x1b, 0x23, 0x21, 0x54, // ESC 2/3 2/1 full row BLB
-      0x1b, 0x23, 0x21, 0x4D, // ESC 2/3 2/1 full row DBH
-      0x8d, 0x83, // DBH, YLF
-    ]);
-    bytes = bytes.concat(this.encodeString("Receiving Bytes"));
-    bytes = bytes.concat(this.crlf());
-    bytes = bytes.concat([
-      0x8c, 0x87, 0x9e, 0x0a, // NSZ, WHF, TRB, APD
-    ]);
-
-    bytes = bytes.concat(this.encodeString("RPT: "));
-    bytes = bytes.concat([
-      0x41, 0x12, 0x45, // "a" RPT 5
-    ]);
-    bytes = bytes.concat(this.crlf());
-
-    bytes = bytes.concat(this.encodeString("APA(6,20): "));
-    bytes = bytes.concat(this.moveTo(6, 20));
-    bytes = bytes.concat(this.encodeString("X"));
-    bytes = bytes.concat(this.crlf());
-
-    bytes = bytes.concat(this.encodeString("Combining umlaut: "));
-    bytes = bytes.concat([
-      0xc8, 0x75, // umlaut, "u"
-    ]);
-    bytes = bytes.concat(this.crlf());
-
-    bytes = bytes.concat(this.encodeString("Parallel: "));
-    bytes = bytes.concat([
-      0x1b, 0x22, 0x41, // ESC 2/2 4/1, switch to parallel C1
-      0x81, 0x92, // RDF, GRB
-    ]);
-    bytes = bytes.concat(this.encodeString("Red on Green"));
-    bytes = bytes.concat([
-      0x87, 0x9e, // WHF, TRB
-    ]);
-    bytes = bytes.concat(this.crlf());
-
-    bytes = bytes.concat([
-      0x1b, 0x23, 0x21, 0x53, // full row attribute yellow background
-      0x80, 0x93, // BKF, YLB
-    ]);
-    bytes = bytes.concat(this.encodeString("Full row black on yellow"));
-    bytes = bytes.concat(this.crlf());
-
-    bytes = bytes.concat([
-      0x1b, 0x22, 0x40, // ESC 2/2 4/0, switch to serial C1
-      0x84, 0x08, 0x9d, 0x08, 0x83, 0x08, // ANB, APB, NBD, APB, ANY, APB
-    ]);
-    bytes = bytes.concat(this.encodeString("Serial yellow on blue"));
-    bytes = bytes.concat(this.crlf());
-
-    bytes = bytes.concat([
-      0x1b, 0x22, 0x40, // ESC 2/2 4/0, switch to serial C1
-      0x87, 0x08, // ANW, APB
-    ]);
-    bytes = bytes.concat(this.encodeString("Mosaic "));
-    bytes = bytes.concat([
-      0x9e, 0x97, 0x39, 0x87, // HMS, MSW, mosaic, ANW
-    ]);
-    bytes = bytes.concat(this.encodeString(" m. hold "));
-    bytes = bytes.concat([
-      0x97, 0x96, 0x93, // MSW, MSC, MSY
-      0x9b, 0x31, 0x40, // CT2
-      0x93, // MSY
-    ]);
-    bytes = bytes.concat(this.crlf());
-
-    bytes = bytes.concat([
-      0x1b, 0x22, 0x41, // ESC 2/2 4/1, switch to parallel C1
-      0x9b, 0x30, 0x40, // CT1
-      0x8c, 0x87, 0x9e, // NSZ, WHF, TRB
-    ]);
-    bytes = bytes.concat(this.encodeString("Flash: "));
-    bytes = bytes.concat([
-      0x88, // FSH
-    ]);
-    bytes = bytes.concat(this.encodeString("flash "));
-    bytes = bytes.concat([
-      0x9b, 0x30, 0x41, // IVF
-    ]);
-    bytes = bytes.concat(this.encodeString("inv. "));
-    bytes = bytes.concat([
-      0x9b, 0x31, 0x41, // RIF
-    ]);
-    bytes = bytes.concat(this.encodeString("red.int. "));
-    bytes = bytes.concat([
-      0x89, 0x88, // STD, FSH
-      0x9b, 0x32, 0x41, // FF1
-    ]);
-    bytes = bytes.concat(this.encodeString("fast1 "));
-    bytes = bytes.concat([
-      0x9b, 0x35, 0x41, // ICF
-    ]);
-    bytes = bytes.concat(this.encodeString("2 "));
-    bytes = bytes.concat([
-      0x9b, 0x35, 0x41, // ICF
-    ]);
-    bytes = bytes.concat(this.encodeString("3 "));
-    bytes = bytes.concat(this.crlf());
-
-
     bytes = bytes.concat([
       0x1b, 0x22, 0x41, // ESC 2/2 4/1, switch to parallel C1
       0x9b, 0x30, 0x40, // CT1
       0x8c, 0x87, 0x9e, 0x89, // NSZ, WHF, TRB, STD
     ]);
-    bytes = bytes.concat(this.moveTo(24, 40-7));
+    bytes = bytes.concat(this.moveTo(24, 40 - 7));
     bytes = bytes.concat(this.encodeString("1234567a"));
+    bytes = bytes.concat(this.moveTo(2, 1));
+
+    // bytes = bytes.concat([
+    //   0x1b, 0x23, 0x21, 0x54, // ESC 2/3 2/1 full row BLB
+    //   0x0a,
+    //   0x1b, 0x23, 0x21, 0x54, // ESC 2/3 2/1 full row BLB
+    //   0x1b, 0x23, 0x21, 0x4D, // ESC 2/3 2/1 full row DBH
+    //   0x8d, 0x83, // DBH, YLF
+    // ]);
+    // bytes = bytes.concat(this.encodeString("Receiving Bytes"));
+    // bytes = bytes.concat(this.crlf());
+    // bytes = bytes.concat([
+    //   0x8c, 0x87, 0x9e, 0x0a, // NSZ, WHF, TRB, APD
+    // ]);
+    //
+    // bytes = bytes.concat(this.encodeString("RPT: "));
+    // bytes = bytes.concat([
+    //   0x41, 0x12, 0x45, // "a" RPT 5
+    // ]);
+    // bytes = bytes.concat(this.crlf());
+    //
+    // bytes = bytes.concat(this.encodeString("APA(6,20): "));
+    // bytes = bytes.concat(this.moveTo(6, 20));
+    // bytes = bytes.concat(this.encodeString("X"));
+    // bytes = bytes.concat(this.crlf());
+    //
+    // bytes = bytes.concat(this.encodeString("Combining umlaut: "));
+    // bytes = bytes.concat([
+    //   0xc8, 0x75, // umlaut, "u"
+    // ]);
+    // bytes = bytes.concat(this.crlf());
+    //
+    // bytes = bytes.concat(this.encodeString("Parallel: "));
+    // bytes = bytes.concat([
+    //   0x1b, 0x22, 0x41, // ESC 2/2 4/1, switch to parallel C1
+    //   0x81, 0x92, // RDF, GRB
+    // ]);
+    // bytes = bytes.concat(this.encodeString("Red on Green"));
+    // bytes = bytes.concat([
+    //   0x87, 0x9e, // WHF, TRB
+    // ]);
+    // bytes = bytes.concat(this.crlf());
+    //
+    // bytes = bytes.concat([
+    //   0x1b, 0x23, 0x21, 0x53, // full row attribute yellow background
+    //   0x80, 0x93, // BKF, YLB
+    // ]);
+    // bytes = bytes.concat(this.encodeString("Full row black on yellow"));
+    // bytes = bytes.concat(this.crlf());
+    //
+    // bytes = bytes.concat([
+    //   0x1b, 0x22, 0x40, // ESC 2/2 4/0, switch to serial C1
+    //   0x84, 0x08, 0x9d, 0x08, 0x83, 0x08, // ANB, APB, NBD, APB, ANY, APB
+    // ]);
+    // bytes = bytes.concat(this.encodeString("Serial yellow on blue"));
+    // bytes = bytes.concat(this.crlf());
+    //
+    // bytes = bytes.concat([
+    //   0x1b, 0x22, 0x40, // ESC 2/2 4/0, switch to serial C1
+    //   0x87, 0x08, // ANW, APB
+    // ]);
+    // bytes = bytes.concat(this.encodeString("Mosaic "));
+    // bytes = bytes.concat([
+    //   0x9e, 0x97, 0x39, 0x87, // HMS, MSW, mosaic, ANW
+    // ]);
+    // bytes = bytes.concat(this.encodeString(" m. hold "));
+    // bytes = bytes.concat([
+    //   0x97, 0x96, 0x93, // MSW, MSC, MSY
+    //   0x9b, 0x31, 0x40, // CT2
+    //   0x93, // MSY
+    // ]);
+    // bytes = bytes.concat(this.crlf());
+    //
+    // bytes = bytes.concat([
+    //   0x1b, 0x22, 0x41, // ESC 2/2 4/1, switch to parallel C1
+    //   0x9b, 0x30, 0x40, // CT1
+    //   0x8c, 0x87, 0x9e, // NSZ, WHF, TRB
+    // ]);
+    // bytes = bytes.concat(this.encodeString("Flash: "));
+    // bytes = bytes.concat([
+    //   0x88, // FSH
+    // ]);
+    // bytes = bytes.concat(this.encodeString("flash "));
+    // bytes = bytes.concat([
+    //   0x9b, 0x30, 0x41, // IVF
+    // ]);
+    // bytes = bytes.concat(this.encodeString("inv. "));
+    // bytes = bytes.concat([
+    //   0x9b, 0x31, 0x41, // RIF
+    // ]);
+    // bytes = bytes.concat(this.encodeString("red.int. "));
+    // bytes = bytes.concat([
+    //   0x89, 0x88, // STD, FSH
+    //   0x9b, 0x32, 0x41, // FF1
+    // ]);
+    // bytes = bytes.concat(this.encodeString("fast1 "));
+    // bytes = bytes.concat([
+    //   0x9b, 0x35, 0x41, // ICF
+    // ]);
+    // bytes = bytes.concat(this.encodeString("2 "));
+    // bytes = bytes.concat([
+    //   0x9b, 0x35, 0x41, // ICF
+    // ]);
+    // bytes = bytes.concat(this.encodeString("3 "));
+    // bytes = bytes.concat(this.crlf());
+
+    bytes = bytes.concat([
+      // 0x1f, 0x23, 0x20,
+      // 0x1f, 0x23, 0x20, 0x20, 0x20, 0x51,
+      // 0x1f, 0x23, 0x20, 0x43,
+      0x1f, 0x23, 0x20, 0x20, 0x20, 0x52, 0x43,
+      0x1f, 0x23, 0x20, 0x20, 0x20, 0x52, 0x43, 0x33, 0x3b, 0x32,
+      0x1f, 0x23, 0x20, 0x20, 0x20, 0x52, 0x43, 0x33, 0x3b, 0x32, 0x42,
+    ]);
+
     bytes = bytes.concat(this.moveTo(24, 1));
     bytes = bytes.concat([
       0x11, // CON
@@ -508,8 +518,8 @@ export default class CeptTest {
         this.cept.input(bytes.shift());
       } else {
         window.clearInterval(i);
-     }
-   }, 10);
+      }
+    }, 10);
 
   }
 }
